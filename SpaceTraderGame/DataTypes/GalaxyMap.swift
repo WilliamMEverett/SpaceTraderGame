e68b2bcf5269fa4ca59cba6fa42cc75d9787e7d7
WilliamMEverett <williammeistereverett@yahoo.com>
2022-06-04 15:04:19 -0600

    Initial Files

---
+++ SpaceTraderGame/DataTypes/GalaxyMap.swift
@@ -0,0 +1,154 @@
+//
+//  GalaxyMap.swift
+//  SpaceTraderGame
+//
+//  Created by William Everett on 6/2/22.
+//
+
+import Foundation
+import CloudKit
+
+class GalaxyMap: Codable {
+    var systemsMap = [Int:StarSystem]()
+    
+    
+    func generateSystems(_ maxSystems : Int) {
+        var resultMap = [Int:StarSystem]()
+        var arrayOfSystems = [StarSystem]()
+        
+        if resultMap.count >= maxSystems {
+            self.systemsMap = resultMap
+            return
+        }
+        
+        var names = self.getMapOfNames()
+        
+        let newStar = StarSystem.generateRandomSystem()
+        newStar.num_id = resultMap.count + 1
+        if let newName = names.randomElement()?.key {
+            newStar.name = newName
+            names.removeValue(forKey: newName)
+        }
+        else {
+            newStar.name = "\(newStar.num_id)"
+        }
+        resultMap[newStar.num_id] = newStar
+        arrayOfSystems.append(newStar)
+        
+        var tries = 0
+        while resultMap.count < maxSystems {
+            let startingSystem = arrayOfSystems.count <= 200 ? arrayOfSystems.randomElement()! : arrayOfSystems[(arrayOfSystems.count - 200)...(arrayOfSystems.count - 1)].randomElement()!
+            if startingSystem.connectingSystems.count >= 3 && tries < 10 {
+                tries += 1
+                continue
+            }
+            tries = 0
+            let newSystem = addNewStarTo(baseSystem: startingSystem, systemMap: resultMap)
+            if newSystem != nil {
+                if let newName = names.randomElement()?.key {
+                    newSystem!.name = newName
+                    names.removeValue(forKey: newName)
+                }
+                
+                resultMap[newSystem!.num_id] = newSystem!
+                arrayOfSystems.append(newSystem!)
+            }
+        }
+        
+        self.systemsMap = resultMap
+        return
+    }
+    
+    private func addNewStarTo(baseSystem : StarSystem, systemMap : [Int:StarSystem]) -> StarSystem? {
+        
+        let distanceFromCore = sqrt(baseSystem.position.x*baseSystem.position.x + baseSystem.position.y*baseSystem.position.y)
+        var angle = Double.random(in: 0...(2*Double.pi))
+        if distanceFromCore > 1 && distanceFromCore < 150 {
+            let currentAngle = atan2(baseSystem.position.y, baseSystem.position.x)
+            angle = currentAngle - Double.pi/2 + Double.random(in:0...Double.pi)
+        }
+        
+        let distance = Double.random(in: 3...10)
+        
+        let xValue = cos(angle)*distance + baseSystem.position.x
+        let yValue = sin(angle)*distance + baseSystem.position.y
+        let zRatio = Double.random(in: 0...1)
+        let zMag = 10*zRatio*zRatio
+        let zValue = Bool.random() ? zMag : -1*zMag
+        let newCoord = Coord(x:xValue,y:yValue,z:zValue)
+        
+        var densityScore : Double = 0
+        var connectedSystems = [(ident:Int,distance:Double)]()
+        systemMap.forEach { (key: Int, value: StarSystem) in
+            if densityScore > 15 || connectedSystems.count > 4 {
+                return
+            }
+            let dist = newCoord.distance(value.position)
+            if dist < 2 {
+                densityScore = 100
+                return
+            }
+            if dist < 12 {
+                densityScore += 12 - dist
+                if value.connectingSystems.count >= 4 {
+                    densityScore = 100
+                    return
+                }
+                connectedSystems.append((ident:key,distance:dist))
+            }
+        }
+        
+        if densityScore > 15 || connectedSystems.count > 4 || connectedSystems.count < 1 {
+            return nil
+        }
+        
+        let sortedConnectingSystems = Array(connectedSystems.sorted { (obj1, obj2 ) -> Bool in
+            return obj1.distance < obj2.distance
+        }.map { $0.ident })
+        
+        var newConnectingSystems = [Int]()
+        sortedConnectingSystems.forEach { key in
+            
+            let otherSystem = systemMap[key]!
+            if Set(newConnectingSystems).intersection(otherSystem.connectingSystems).isEmpty {
+                newConnectingSystems.append(key)
+            }
+        }
+        
+        let newSystem = StarSystem.generateRandomSystem()
+        newSystem.num_id = systemMap.count + 1
+        newSystem.name = "\(newSystem.num_id)"
+        newSystem.connectingSystems = newConnectingSystems
+        newSystem.position = newCoord
+    
+        newConnectingSystems.forEach { key in
+            let otherSystem = systemMap[key]!
+            otherSystem.connectingSystems.append(newSystem.num_id)
+        }
+        
+        return newSystem
+    }
+    
+    private func getMapOfNames() -> [String:Bool] {
+        var result = [String:Bool]()
+        var fileContents : String? = nil
+        if let fileURL = Bundle.main.url(forResource: "city_list", withExtension: "txt") {
+            fileContents = try? String(contentsOf: fileURL)
+        }
+        
+        if fileContents == nil {
+            print("Could not open name list file")
+            return result
+        }
+        
+        let lines = fileContents!.split(separator: "\n")
+        lines.forEach { ln in
+            let trimmed = ln.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+            if !trimmed.isEmpty {
+                result[trimmed] = true
+            }
+        }
+        
+        return result
+    }
+}
---
+++ SpaceTraderGame/DataTypes/StarSystem.swift
@@ -0,0 +1,205 @@
+//
+//  StarSystem.swift
+//  SpaceTraderGame
+//
+//  Created by William Everett on 5/31/22.
+//
+
+import Foundation
+
+enum StarSystemEconomy : Int, CustomStringConvertible, Codable {
+    case none
+    case mining
+    case agriculture_luxury
+    case agriculture_basic
+    case industrial
+    case post_industrial
+    case resort
+    
+    var description : String {
+        switch self {
+        case .none: return "None"
+        case .mining: return "Mining"
+        case .agriculture_luxury: return "Agriculture (Luxury)"
+        case .agriculture_basic: return "Agriculture (Basic)"
+        case .industrial: return "Industrial"
+        case .post_industrial: return "Post-Industrial"
+        case .resort: return "Resort"
+        }
+    }
+    
+}
+
+enum StarSystemStage : Int, CustomStringConvertible, Codable {
+    case empty
+    case colonial
+    case emerging
+    case apex
+    case declining
+    
+    var description : String {
+        switch self {
+        case .empty: return "Empty"
+        case .colonial: return "Colonial"
+        case .emerging: return "Emerging"
+        case .apex: return "Apex"
+        case .declining: return "Declining"
+        }
+    }
+    
+}
+
+struct Coord : Codable, CustomStringConvertible {
+    var x : Double = 0
+    var y : Double = 0
+    var z : Double = 0
+    
+    func distance(_ otherCoord : Coord) -> Double {
+        let xDist = self.x - otherCoord.x
+        let yDist = self.y - otherCoord.y
+        let zDist = self.z - otherCoord.z
+        return sqrt(xDist*xDist + yDist*yDist + zDist*zDist)
+    }
+    
+    var description : String {
+        let xString = String(format: "%.1f", self.x)
+        let yString = String(format: "%.1f", self.y)
+        let zString = String(format: "%.1f", self.z)
+        return "(\(xString),\(yString),\(zString))"
+    }
+}
+
+class StarSystem : Codable, CustomStringConvertible {
+    
+    var num_id = 0
+    var name = ""
+    var position = Coord()
+    var economy = StarSystemEconomy.none
+    var stage = StarSystemStage.empty
+    var population = 0
+    var danger = 0
+    var faction = 0
+    var connectingSystems : [Int] = []
+    
+    var populationDescription : String {
+        if self.population < 1000000 {
+            return "\(String(format: "%.1f", (Double(self.population)/1000)))k"
+        }
+        else if self.population < 1000000000 {
+            return "\(String(format: "%.1f", (Double(self.population)/1000000)))M"
+        }
+        else {
+            return "\(String(format: "%.1f", (Double(self.population)/1000000000)))B"
+        }
+    }
+    
+    var description : String {
+        return "\(self.num_id).  \(self.name)    \(self.position)\n\(self.stage)  \(self.economy)\nPop: \(self.populationDescription)   Danger: \(self.danger)   Faction:\(self.faction)\nConnecting Systems: \(self.connectingSystems))\n"
+    }
+    
+    class func generateRandomSystem() -> StarSystem {
+        let returnValue = StarSystem()
+        
+        returnValue.stage = self.getRandomStage()
+        
+        returnValue.economy = self.getRandomEconomy(returnValue)
+        
+        switch returnValue.stage {
+        case .empty:
+            returnValue.population = 0
+            returnValue.danger = Int.random(in: 5...10)
+        case .colonial:
+            returnValue.population = Int.random(in: 10..<1000)*1000
+            returnValue.danger = Int.random(in: 3...9)
+        case .emerging:
+            returnValue.population = Int.random(in: 1..<1000)*1000000
+            returnValue.danger = Int.random(in: 1...5)
+        case .apex:
+            returnValue.population = Int.random(in: 1..<500)*1000000000
+            returnValue.danger = Int.random(in: 0...3)
+        case .declining:
+            returnValue.population = Int.random(in: 1..<10000)*1000000
+            returnValue.danger = Int.random(in: 4...9)
+        }
+        
+        return returnValue
+    }
+    
+    private class func getRandomStage() -> StarSystemStage {
+        let randoStage = Int.random(in: 1...100)
+        switch randoStage {
+        case 1...5:
+            return .empty
+        case 6...30:
+            return .colonial
+        case 31...60:
+            return .emerging
+        case 61...80:
+            return .apex
+        default:
+            return .declining
+        }
+    }
+    
+    private class func getRandomEconomy(_ system : StarSystem) -> StarSystemEconomy {
+        let randoEconomy = Int.random(in: 1...100)
+        switch system.stage {
+        case .empty:
+            return .none
+        case .colonial:
+            switch randoEconomy {
+            case 1...40:
+                return .mining
+            case 41...80:
+                return .agriculture_basic
+            default:
+                return .agriculture_luxury
+            }
+        case .emerging:
+            switch randoEconomy {
+            case 1...30:
+                return .mining
+            case 31...50:
+                return .agriculture_basic
+            case 51...70:
+                return .agriculture_luxury
+            case 71...95:
+                return .industrial
+            default:
+                return .resort
+            }
+        case .apex:
+            switch randoEconomy {
+            case 1...5:
+                return .mining
+            case 6...15:
+                return .agriculture_basic
+            case 16...30:
+                return .agriculture_luxury
+            case 31...65:
+                return .industrial
+            case 66...95:
+                return .post_industrial
+            default:
+                return .resort
+            }
+        case .declining:
+            switch randoEconomy {
+            case 1...10:
+                return .mining
+            case 11...15:
+                return .agriculture_basic
+            case 16...20:
+                return .agriculture_luxury
+            case 21...50:
+                return .industrial
+            case 51...95:
+                return .post_industrial
+            default:
+                return .resort
+            }
+        }
+    }
+    
+}
+
