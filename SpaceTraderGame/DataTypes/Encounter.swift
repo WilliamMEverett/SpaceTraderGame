908d916564b30d223cba9923724bdcda3695641c
WilliamMEverett <williammeistereverett@yahoo.com>
2022-06-21 17:06:45 -0600

    Work on encounters

---
+++ SpaceTraderGame/DataTypes/Encounter.swift
@@ -0,0 +1,54 @@
+//
+//  Encounter.swift
+//  SpaceTraderGame
+//
+//  Created by William Everett on 6/20/22.
+//
+
+import Foundation
+
+enum EncounterType : Int, Codable, CustomStringConvertible {
+    case pirate
+    case merchant
+    case patrol
+    case other
+    
+    var description : String {
+        switch self {
+        case .pirate: return "Pirate"
+        case .merchant: return "Merchant"
+        case .patrol: return "System Patrol"
+        case .other: return "Unknown"
+        }
+    }
+}
+
+class Encounter : Codable {
+    
+    var ship : Ship? = nil
+    var combatSkill : Int = 0
+    var navigationSkill : Int = 0
+    var type : EncounterType = .pirate
+    
+    class func checkForEncounterInCurrentSystem(player: Player, map: GalaxyMap) -> Encounter? {
+        guard let currentSystem = map.getSystemForId(player.location) else {
+            return nil
+        }
+        if currentSystem.danger == 0 {
+            return nil
+        }
+        
+        //Note: this is for a pirate encounter. Probability of this happening should depend on danger level, for now, always happens
+        let playerShipThreat = player.ship.threatLevel()
+        let minimumThreat = playerShipThreat < 2 ? 0 : playerShipThreat - 2
+        let maxThreat = max(max(playerShipThreat + 2, currentSystem.danger), player.reputation/10)
+        let enemyDanger = Int.random(in: minimumThreat...maxThreat)
+        
+        let enc = Encounter()
+        enc.ship = Ship.shipForThreatLevel(enemyDanger)
+        enc.combatSkill = Int.random(in: (min(0,player.combat - 30))...(max(player.combat - 10,currentSystem.danger*10)))
+        enc.navigationSkill = max(0,min(100,enc.combatSkill + Int.random(in: -20...20)))
+        
+        return enc
+    }
+}
--- SpaceTraderGame/DataTypes/Ship.swift
+++ SpaceTraderGame/DataTypes/Ship.swift
@@ -48,6 +48,63 @@ class Ship : Codable {
         return ret
     }
     
+    class func shipForThreatLevel(_ level : Int) -> Ship {
+        let ret = Ship()
+        ret.hull = 50
+        ret.cargo = 50
+        ret.engine = 50
+        ret.fuel = 50
+        
+        let weap = ShipEquipment()
+        weap.weight = 10
+        weap.strength = 10
+        weap.type = .weapon
+        ret.equipment.append(weap)
+        
+        if level <= 1 {
+            return ret
+        }
+        var currentThreatLevel = ret.threatLevel()
+        
+        while currentThreatLevel < level {
+            let option = Int.random(in: 0...5)
+            switch option {
+            case 0:
+                ret.engine += 50
+            case 1:
+                let weap = ShipEquipment()
+                weap.weight = 10
+                weap.strength = 10
+                weap.type = .weapon
+                ret.equipment.append(weap)
+            case 2,3:
+                let existingWeapon = ret.equipment.filter({$0.type == .weapon}).randomElement()
+                existingWeapon?.strength += 10
+                existingWeapon?.weight += 10
+            case 4:
+                if let existingShield = ret.equipment.filter({$0.type == .shield}).randomElement() {
+                    existingShield.strength += 50
+                    existingShield.weight += 10
+                }
+                else {
+                    let newShield = ShipEquipment()
+                    newShield.weight = 10
+                    newShield.strength = 50
+                    newShield.type = .shield
+                    ret.equipment.append(newShield)
+                }
+            default:
+                ret.hull += 50
+            }
+            if ret.totalCargoWeight() > ret.cargo {
+                ret.cargo = ret.totalCargoWeight()
+            }
+        
+            currentThreatLevel = ret.threatLevel()
+        }
+        return ret
+    }
+    
     class func timeToLeaveDock() -> Double {
         return 0.25
     }
@@ -72,5 +129,29 @@ class Ship : Codable {
     
     func fuelToTravelTime(time: Double) -> Double {
         return time*self.engine/50
+    }
+    
+    func totalWeaponStrength() -> Double {
+        return self.equipment.reduce(0.0) { partialResult, value in
+            return value.type == .weapon ? partialResult + value.strength : partialResult
+        }
+    }
+    
+    func totalShieldStrength() -> Double {
+        return self.equipment.reduce(0.0) { partialResult, value in
+            return value.type == .shield ? partialResult + value.strength : partialResult
+        }
+    }
+    
+    func threatLevel() -> Int {
+        
+        let defensiveTotal = (self.hull + self.totalShieldStrength())/10.0
+        let offensiveTotal = self.totalWeaponStrength()
+        if offensiveTotal == 0 {
+            return 0
+        }
+        let maneuverTotal = self.engine*10/self.hull
+        
+        return Int(floor(offensiveTotal/10) + floor((defensiveTotal + maneuverTotal)/20))
     }
 }
