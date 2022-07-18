5820a78aa0f560fa6dbceb9d414836b83030e27f
WilliamMEverett <williammeistereverett@yahoo.com>
2022-07-17 18:54:03 -0600

    Implemented Loading a game

--- SpaceTraderGame/MainViewController.swift
+++ SpaceTraderGame/MainViewController.swift
@@ -12,6 +12,8 @@ class MainViewController: NSViewController {
     
     var gameViewController : GameViewController?
     var newGameWindowController : NewGameWindowController?
+    var savePanel : NSSavePanel?
+    var loadPanel : NSOpenPanel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -61,20 +63,21 @@ class MainViewController: NSViewController {
         }
         let fileName = String(format: "%@_%0.1f", validatedName, self.gameViewController!.gameState.time).replacingOccurrences(of: ".", with: "_")
         
-        let savePanel = NSSavePanel()
-        savePanel.nameFieldStringValue = fileName
-        savePanel.title = "Save Game"
-        savePanel.showsHiddenFiles = false
-        savePanel.allowsOtherFileTypes = false
+        self.savePanel = NSSavePanel()
+        self.savePanel!.nameFieldStringValue = fileName
+        self.savePanel!.title = "Save Game"
+        self.savePanel!.showsHiddenFiles = false
+        self.savePanel!.allowsOtherFileTypes = false
         
-        savePanel.allowedContentTypes = [UTType("com.shinybuttonsoftware.spacetrader")!]
+        self.savePanel!.allowedContentTypes = [UTType("com.shinybuttonsoftware.spacetrader")!]
         
         let currentGameState = self.gameViewController!.gameState!
         
-        savePanel.beginSheetModal(for: self.view.window!) { response in
-            if response == .OK && savePanel.url != nil {
-                weakSelf?.saveGame(game: currentGameState, location: savePanel.url!)
+        self.savePanel!.beginSheetModal(for: self.view.window!) { response in
+            if response == .OK && weakSelf?.savePanel?.url != nil {
+                weakSelf?.saveGame(game: currentGameState, location: weakSelf!.savePanel!.url!)
             }
+            weakSelf?.savePanel = nil
         }
     }
     
@@ -91,29 +94,76 @@ class MainViewController: NSViewController {
         else {
             print("Failed to save game")
         }
+    }
+    
+    private func showLoadGameDialog() {
+
+        weak var weakSelf = self
         
+        self.loadPanel = NSOpenPanel()
+        self.loadPanel!.title = "Load Game"
+        self.loadPanel!.showsHiddenFiles = false
+        self.loadPanel!.allowsOtherFileTypes = false
         
+        self.loadPanel!.allowedContentTypes = [UTType("com.shinybuttonsoftware.spacetrader")!]
+        
+        self.loadPanel!.beginSheetModal(for: self.view.window!) { response in
+            if response == .OK && weakSelf?.loadPanel?.url != nil {
+                weakSelf?.loadGame(location: weakSelf!.loadPanel!.url!)
+            }
+            weakSelf?.loadPanel = nil
+        }
+    }
+    
+    private func loadGame(location : URL) {
+        guard let data = FileManager.default.contents(atPath: location.path) else {
+            print("Invalid file at \(location)")
+            return
+        }
+        guard let newGameState = try? JSONDecoder().decode(GameState.self, from: data) else {
+            print("Invalid json structure in file at \(location)")
+            return
+        }
+        
+        newGameState.saved = true
+        
+        self.gameViewController?.view.removeFromSuperview()
+        self.gameViewController?.removeFromParent()
+        
+        self.gameViewController = GameViewController()
+        self.gameViewController!.gameState = newGameState
+        
+        self.addChild(self.gameViewController!)
+        self.gameViewController!.view.frame = self.view.bounds
+        self.view.addSubview(gameViewController!.view)
+        
+    }
+    
+    private func showUnsavedConfirmation(_ completion: @escaping ()->Void) {
+        let al = NSAlert()
+        al.alertStyle = .informational
+        al.messageText = "Game in Progress"
+        al.informativeText = "You have an unsaved game in progress. Continuing will end this game."
+        al.addButton(withTitle: "OK")
+        al.addButton(withTitle: "Cancel")
+        al.beginSheetModal(for: self.view.window!) { response in
+            if response == .alertFirstButtonReturn {
+                completion()
+            }
+        }
     }
     
     //MARK: - Actions
     
     @IBAction func newMenuItem(_ sender : AnyObject?) {
-        if self.newGameWindowController != nil {
+        if self.newGameWindowController != nil || self.savePanel != nil || self.loadPanel != nil {
             return
         }
         
+        weak var weakSelf = self
         if self.gameViewController?.gameState.gameOver == false && self.gameViewController?.gameState.saved == false {
-            let al = NSAlert()
-            al.alertStyle = .informational
-            al.messageText = "Game in Progress"
-            al.informativeText = "You have an unsaved game in progress. Continuing will end this game."
-            al.addButton(withTitle: "OK")
-            al.addButton(withTitle: "Cancel")
-            weak var weakSelf = self
-            al.beginSheetModal(for: self.view.window!) { response in
-                if response == .alertFirstButtonReturn {
-                    weakSelf?.showNewGameDialog()
-                }
+            self.showUnsavedConfirmation {
+                weakSelf?.showNewGameDialog()
             }
         }
         else {
@@ -122,15 +172,25 @@ class MainViewController: NSViewController {
     }
     
     @IBAction func saveMenuItem(_ sender : AnyObject?) {
-        if self.newGameWindowController != nil {
+        if self.newGameWindowController != nil || self.savePanel != nil || self.loadPanel != nil {
             return
         }
         self.showSaveGameDialog()
     }
     
     @IBAction func loadMenuItem(_ sender : AnyObject?) {
-        if self.newGameWindowController != nil {
+        if self.newGameWindowController != nil || self.savePanel != nil || self.loadPanel != nil {
             return
+        }
+        
+        weak var weakSelf = self
+        if self.gameViewController?.gameState.gameOver == false && self.gameViewController?.gameState.saved == false {
+            self.showUnsavedConfirmation {
+                weakSelf?.showLoadGameDialog()
+            }
+        }
+        else {
+            self.showLoadGameDialog()
         }
     }
 
